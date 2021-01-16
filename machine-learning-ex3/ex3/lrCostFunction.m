abbeb0dbe5fe10f9c14efe086e2d487111adff34
xykong <xy.kong@gmail.com>
2021-01-16 22:18:55 +0800

    ex3: finish 1.3.3 Vectorizing regularized logistic regression

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,13 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
 
+theta(1) = 0;
 
+J = (1 / m) * (-y' * log(h) - (1 - y)' * log(1 - h)) + lambda / (2 * m) * theta' * theta;
 
-
-
-
-
+grad = 1 / m * (X' * (h - y)) + ( lambda / m ) * theta;
 
 
 % =============================================================
