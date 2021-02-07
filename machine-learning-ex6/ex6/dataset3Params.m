7944b89e80777a41d0299c37ecf6cc5f2eb502c6
xykong <xy.kong@gmail.com>
2021-02-07 11:44:08 +0800

    ex6: finish 1.2.3 Example Dataset 3. Replace with the best result

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -24,30 +24,33 @@ sigma = 0.3;
 %
 
 
-vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
-error_min = inf;
+% vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+% error_min = inf;
 
-for C = vec
-  for sigma = vec
-    fprintf('Try next C, sigma = %f, %f\n', C, sigma);
+% for C = vec
+%   for sigma = vec
+%     fprintf('Try next C, sigma = %f, %f\n', C, sigma);
 
-    model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-    error   = mean(double(svmPredict(model, Xval) ~= yval));
-    if( error <= error_min )
-      C_final     = C;
-      sigma_final = sigma;
-      error_min   = error;
+%     model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%     error   = mean(double(svmPredict(model, Xval) ~= yval));
+%     if( error <= error_min )
+%       C_final     = C;
+%       sigma_final = sigma;
+%       error_min   = error;
 
-      fprintf('Found better C, sigma = %f, %f with error = %f\n', C_final, sigma_final, error_min)
-    end
-  end
-end
+%       fprintf('Found better C, sigma = %f, %f with error = %f\n', C_final, sigma_final, error_min)
+%     end
+%   end
+% end
 
-C     = C_final;
-sigma = sigma_final;
+% C     = C_final;
+% sigma = sigma_final;
 
-fprintf('The best C, sigma = [%f %f] with error = %f\n', C, sigma, error_min);
+% fprintf('The best C, sigma = [%f %f] with error = %f\n', C, sigma, error_min);
 
+% Replace with the best result
+C     = 1.0;
+sigma = 0.1;
 
 % =========================================================================
 
